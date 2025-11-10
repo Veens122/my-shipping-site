b0c2c93a169492725969164ef726ca0d16f2f3ad
Isika Ugochukwu Vincent <Uvincent54@gmail.com>
2025-11-10 12:05:22 +0100

    Favicon updated again

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,10 +29,10 @@
     <meta name="twitter:image" content="preview.html">
 
     <!-- FAVICON FILES -->
-    <link href="ico/apple-touch-icon-144-precomposed.png" rel="apple-touch-icon" sizes="144x144">
-    <link href="ico/apple-touch-icon-114-precomposed.png" rel="apple-touch-icon" sizes="114x114">
-    <link href="ico/apple-touch-icon-72-precomposed.png" rel="apple-touch-icon" sizes="72x72">
-    <link href="ico/apple-touch-icon-57-precomposed.png" rel="apple-touch-icon">
+    <link href="{{ asset('assets/ico/favicon.png') }}" rel="apple-touch-icon" sizes="144x144">
+    <link href="{{ asset('assets/ico/favicon.png') }}" rel="apple-touch-icon" sizes="114x114">
+    <link href="{{ asset('assets/ico/favicon.png') }}" rel="apple-touch-icon" sizes="72x72">
+    <link href="{{ asset('assets/ico/favicon.png') }}" rel="apple-touch-icon">
     <link href="{{ asset('assets/ico/favicon.png') }}" rel="shortcut icon">
 
     <!-- CSS FILES -->
